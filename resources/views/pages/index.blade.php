392e00e02b52fb74b223e59d70e7e997d764fa1d
Joseph Tandingan <josephdevapps@gmail.com>
2018-07-25 13:42:50 +0800

    upload to git

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+            <h1>{{$title}}</h1>
+            <p>This is the laravel application from the "Laravel From Scratch" created by Joseph Herbert Tandingan
+
+            </p>
+            @if(Auth::guest())
+                <p>
+                    <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+                    <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+                </p>
+            @endif
+    </div>
+@endsection
